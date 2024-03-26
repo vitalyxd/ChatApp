2b57b51145918f99819d1020438dea194b461ea9
Vito <135276365+vitalyxd@users.noreply.github.com>
2024-03-26 20:40:14 +0100

    Add files via upload

---
+++ ChatAppNiOP/AppNiOP/ChatAplikacijaNiOP/Form4.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace ChatAplikacijaNiOP
+{
+    partial class Form4
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.PosaljiPoruku = new System.Windows.Forms.Button();
+            this.Poruka = new System.Windows.Forms.TextBox();
+            this.Poruke = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // PosaljiPoruku
+            // 
+            this.PosaljiPoruku.Location = new System.Drawing.Point(438, 281);
+            this.PosaljiPoruku.Name = "PosaljiPoruku";
+            this.PosaljiPoruku.Size = new System.Drawing.Size(128, 23);
+            this.PosaljiPoruku.TabIndex = 0;
+            this.PosaljiPoruku.Text = "PosaljiPoruku";
+            this.PosaljiPoruku.UseVisualStyleBackColor = true;
+            // 
+            // Poruka
+            // 
+            this.Poruka.Location = new System.Drawing.Point(320, 283);
+            this.Poruka.Name = "Poruka";
+            this.Poruka.Size = new System.Drawing.Size(100, 20);
+            this.Poruka.TabIndex = 1;
+            // 
+            // Poruke
+            // 
+            this.Poruke.FormattingEnabled = true;
+            this.Poruke.Location = new System.Drawing.Point(320, 177);
+            this.Poruke.Name = "Poruke";
+            this.Poruke.Size = new System.Drawing.Size(120, 95);
+            this.Poruke.TabIndex = 2;
+            // 
+            // Form4
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Poruke);
+            this.Controls.Add(this.Poruka);
+            this.Controls.Add(this.PosaljiPoruku);
+            this.Name = "Form4";
+            this.Text = "Form4";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button PosaljiPoruku;
+        private System.Windows.Forms.TextBox Poruka;
+        private System.Windows.Forms.ListBox Poruke;
+    }
+}
---
+++ ChatAppNiOP/AppNiOP/ChatAplikacijaNiOP/PodaktovniKontekst.cs
@@ -0,0 +1,148 @@
+﻿using System;
+
+public class PodatkovniKontekst
+{
+    private List<Korisnik> korisnici;
+    private List<Poruka> poruke;
+    private List<Razgovor> razgovori;
+
+    public List<Korisnik> Korisnici { get { return this.korisnici; } }
+    public List<Poruka> Poruke { get { return this.poruke; } }
+    public List<Razgovor> Razgovori { get { return this.razgovori; } }
+
+    private string datKorisnici = "korisnici.dat";
+    private string datPoruke = "poruke.dat";
+    private string datRazgovori = "razgovori.dat";
+
+    public PodatkovniKontekst()
+    {
+        this.korisnici = UcitajKorisnike();
+        this.poruke = UcitajPoruke();
+        this.razgovori = UcitajRazgovore();
+    }
+
+    private List<Korisnik> UcitajKorisnike()
+    {
+        List<Korisnik> korisnici = new List<Korisnik>();
+        if (File.Exists(datKorisnici))
+        {
+            using (StreamReader sr = new StreamReader(datKorisnici))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string linija = sr.ReadLine();
+                    string[] polja = linija.Split('|');
+                    Korisnik k = new Korisnik();
+                    k.OIB = polja[0];
+                    k.Ime = polja[1];
+                    k.Prezime = polja[2];
+                    k.Telefon = polja[3];
+                    korisnici.Add(k);
+                }
+            }
+        }
+        return korisnici;
+    }
+
+    private List<Poruka> UcitajPoruke()
+    {
+        List<Poruka> poruke = new List<Poruka>();
+        if (File.Exists(datPoruke))
+        {
+            using (StreamReader sr = new StreamReader(datPoruke))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string linija = sr.ReadLine();
+                    string[] polja = linija.Split('|');
+                    Poruka p = new Poruka();
+                    p.OIB = polja[0];
+                    p.Naslov = polja[1];
+                    p.Sadrzaj = polja[2];
+                    poruke.Add(p);
+                }
+            }
+        }
+        return poruke;
+    }
+
+    private List<Razgovor> UcitajRazgovore()
+    {
+        List<Razgovor> razgovori = new List<Razgovor>();
+        if (File.Exists(datRazgovori))
+        {
+            using (StreamReader sr = new StreamReader(datRazgovori))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string linija = sr.ReadLine();
+                    string[] polja = linija.Split('|');
+                    Razgovor r = new Razgovor();
+                    r.Id = Convert.ToInt32(polja[0]);
+                    r.Datum = polja[1];
+                    r.Vrijeme = polja[2];
+                    razgovori.Add(r);
+                }
+            }
+        }
+        return razgovori;
+    }
+
+    public void SpremiKorisnike()
+    {
+        using (StreamWriter sw = new StreamWriter(datKorisnici))
+        {
+            foreach (Korisnik k in this.Korisnici)
+            {
+                sw.WriteLine("{0}|{1}|{2}|{3}",
+                    k.OIB, k.Ime, k.Prezime, k.Telefon);
+            }
+        }
+    }
+
+    public void SpremiPoruke()
+    {
+        using (StreamWriter sw = new StreamWriter(datPoruke))
+        {
+            foreach (Poruka p in this.Poruke)
+            {
+                sw.WriteLine("{0}|{1}|{2}",
+                    p.OIB, p.Naslov, p.Sadrzaj);
+            }
+        }
+    }
+
+    public void SpremiRazgovore()
+    {
+        using (StreamWriter sw = new StreamWriter(datRazgovori))
+        {
+            foreach (Razgovor r in this.Razgovori)
+            {
+                sw.WriteLine("{0}|{1}|{2}",
+                    r.Id, r.Datum, r.Vrijeme);
+            }
+        }
+    }
+}
+
+public class Korisnik
+{
+    public string OIB { get; set; }
+    public string Ime { get; set; }
+    public string Prezime { get; set; }
+    public string Telefon { get; set; }
+}
+
+public class Poruka
+{
+    public string OIB { get; set; }
+    public string Naslov { get; set; }
+    public string Sadrzaj { get; set; }
+}
+
+public class Razgovor
+{
+    public int Id { get; set; }
+    public string Datum { get; set; }
+    public string Vrijeme { get; set; }
+}
